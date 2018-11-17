30bece4b3f50c0d8f3d6a7f33e4326c9d5d99755
Remy Frenoy <rfrenoy@gmail.com>
2018-11-17 14:43:33 +0100

    write short docstring explaining lib.py

--- lib.py
+++ lib.py
@@ -2,4 +2,11 @@ from future.builtins import round
 
 
 def my_round(x):
+    """
+    The built-in round function behaves differently in Python 2.7 and Python 3.6. We hence use this little
+    (useless) function as the code we want the CI to test on the two major python versions
+    and make sure our code behaves the same (thanks to the future import).
+    :param x: a numerical value
+    :return: the rounded value
+    """
     return round(x)
